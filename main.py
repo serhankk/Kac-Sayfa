cca84099bb4155ca31f4e76bd2524143aeaedaf3
serhankk <serhankonukluk@gmail.com>
2022-03-31 13:14:47 +0300

    sayfa sayisini bulamama durumuyla ilgili guncelleme yapildi

--- main.py
+++ main.py
@@ -8,32 +8,47 @@ import requests
 headers_param = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
 
-USER_INPUT = str(input("Yazar ve Kitap Adı: ")).replace(" ", "+")
-SEARCH = "https://www.google.com/search?q=" + USER_INPUT + "+kac+sayfa"
-print("Sorgu\t\t  : " + SEARCH)
+print("Kaç-Sayfa".center(50, '-'))
+print("""Hoşgeldiniz. Lütfen kaç sayfa olduğunu
+öğrenmek istediğiniz kitabın adını ve daha tutarlı
+bilgi gelmesi için yazar adını da giriniz. 
+Ardından program sizin için arama yapacaktır.
 
-response = requests.get(SEARCH, headers=headers_param)
-soup = BeautifulSoup(response.content, "lxml", from_encoding="utf-8")
+Programdan çıkmak için 0 yazmanız yeterlidir.""")
+print("-" * 50)
 
-BOOK_INFORMATION_FLAG = False
-PAPER_FLAG = False
+while True:
+    USER_INPUT = str(input("Yazar ve Kitap Adı: ")).replace(" ", "+")
+    if USER_INPUT == "0":
+        print("Hoşçakalın!")
+        exit("-" * 50)
 
-try:
-    book_name = soup.find("div", attrs={"class": "iKJnec"}).text
-    KITAP_FLAG = True
-    print("Kitap Bilgisi\t  : " + book_name)
-except AttributeError:
-    print("Kitap Bilgisi\t  :", None)
+    SEARCH = "https://www.google.com/search?q=" + USER_INPUT + "+kac+sayfa"
+    print("Sorgu\t\t  : " + SEARCH)
 
-book_information = soup.find_all("div", attrs={"class": "Crs1tb"})
+    response = requests.get(SEARCH, headers=headers_param)
+    soup = BeautifulSoup(response.content, "lxml", from_encoding="utf-8")
 
-for i, _ in enumerate(book_information):
-    tds = book_information[i].find_all("td")
-    for j, _ in enumerate(tds):
-        if tds[j].text ==  "Sayfa Sayısı:":
-            book_paper = tds[j + 1].text
-            PAPER_FLAG = True
-            print("Sayfa Sayısı\t  : " + book_paper)
+    BOOK_INFORMATION_FLAG = False
+    PAPER_FLAG = False
 
-if BOOK_INFORMATION_FLAG is False and PAPER_FLAG is False:
-    print('Kitap bilgisi bulunamadı..')
+    try:
+        book_name = soup.find("div", attrs={"class": "iKJnec"}).text
+        KITAP_FLAG = True
+        print("Kitap Bilgisi\t  : " + book_name)
+    except AttributeError:
+        print("Kitap Bilgisi\t  :", None)
+
+    book_information = soup.find_all("div", attrs={"class": "Crs1tb"})
+
+    for i, _ in enumerate(book_information):
+        tds = book_information[i].find_all("td")
+        for j, _ in enumerate(tds):
+            if tds[j].text ==  "Sayfa Sayısı:" or tds[j].text ==  "Sayfa":
+                book_paper = tds[j + 1].text
+                PAPER_FLAG = True
+                print("Sayfa Sayısı\t  : " + book_paper)
+
+    if BOOK_INFORMATION_FLAG is False and PAPER_FLAG is False:
+        print('Kitap bilgisi bulunamadı..')
+    print("-" * 50)
