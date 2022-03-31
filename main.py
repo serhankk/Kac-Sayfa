5170d47c7a06d18e64d79513bb0e3d6bdcf670f6
serhankk <serhankonukluk@gmail.com>
2022-03-31 12:07:59 +0300

    estetik düzenlemeler yapildi

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ headersparam = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWe
 
 USER_INPUT = str(input("Yazar ve kitap adı: ")).replace(" ", "+")
 SEARCH = "https://www.google.com/search?q=" + USER_INPUT + "+kac+sayfa"
-print("Sorgu: ", SEARCH)
+print("Sorgu\t\t  : " + SEARCH)
 
 r = requests.get(SEARCH, headers=headersparam)
 soup = BeautifulSoup(r.content, "lxml", from_encoding="utf-8")
 
 try:
     kitap_adi = soup.find("div", attrs={"class": "iKJnec"}).text
-    print("Kitap Bilgisi: ", kitap_adi)
+    print("Kitap Bilgisi\t  : " + kitap_adi)
 except AttributeError:
     kitap_adi = ''
 kunye = soup.find_all("div", attrs={"class": "Crs1tb"})
@@ -26,7 +26,7 @@ for i in range(len(kunye)):
     tds = kunye[i].find_all("td")
     for j in range(len(tds)):
         if tds[j].text ==  "Sayfa Sayısı:":
-            print("Sayfa sayısı: %s" % tds[j + 1].text)
+            print("Sayfa sayısı\t  : %s" % tds[j + 1].text)
 
 
 
