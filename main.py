4b75b426806a835c09e8944af51fcc2527695120
serhankk <serhankonukluk@gmail.com>
2022-03-31 12:38:47 +0300

    kodun statik analizi yapildi

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-
+"""
+Girilen yazar ve kitap adına göre
+kitabın kaç sayfa olduğunu döndürür.
+"""
 from bs4 import BeautifulSoup
 import requests
-import lxml
 
-
-headersparam = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
+headers_param = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
 
 USER_INPUT = str(input("Yazar ve Kitap Adı: ")).replace(" ", "+")
 SEARCH = "https://www.google.com/search?q=" + USER_INPUT + "+kac+sayfa"
 print("Sorgu\t\t  : " + SEARCH)
 
-r = requests.get(SEARCH, headers=headersparam)
-soup = BeautifulSoup(r.content, "lxml", from_encoding="utf-8")
+response = requests.get(SEARCH, headers=headers_param)
+soup = BeautifulSoup(response.content, "lxml", from_encoding="utf-8")
 
 BOOK_INFORMATION_FLAG = False
 PAPER_FLAG = False
@@ -21,26 +23,17 @@ try:
     KITAP_FLAG = True
     print("Kitap Bilgisi\t  : " + book_name)
 except AttributeError:
-    book_name = None
-    print("Kitap Bilgisi\t  :", book_name)
+    print("Kitap Bilgisi\t  :", None)
 
 book_information = soup.find_all("div", attrs={"class": "Crs1tb"})
 
-for i in range(len(book_information)):
+for i, _ in enumerate(book_information):
     tds = book_information[i].find_all("td")
-    for j in range(len(tds)):
+    for j, _ in enumerate(tds):
         if tds[j].text ==  "Sayfa Sayısı:":
             book_paper = tds[j + 1].text
             PAPER_FLAG = True
-            print("Sayfa Sayısı\t  : %s" % book_paper)
+            print("Sayfa Sayısı\t  : " + book_paper)
 
-if BOOK_INFORMATION_FLAG == False and PAPER_FLAG == False:
+if BOOK_INFORMATION_FLAG is False and PAPER_FLAG is False:
     print('Kitap bilgisi bulunamadı..')
-
-
-
-
-
-
-
-
